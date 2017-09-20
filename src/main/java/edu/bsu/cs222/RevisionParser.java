311b36ffd931042ab07d489820cad1adaccfbecb
Allie Curtis <accurtis@bsu.edu>
2017-09-20 16:50:32 -0400

    Created simple RevisionParser and learning tests, created Revision class with blank constructor.

--- src/main/java/edu/bsu/cs222/RevisionParser.java
+++ src/main/java/edu/bsu/cs222/RevisionParser.java
@@ -1,4 +1,31 @@
 package edu.bsu.cs222;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Map;
+
 public class RevisionParser {
+
+    public String parser(String field) {
+        JsonParser parser = new JsonParser();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("testResource.json");
+        Reader reader = new InputStreamReader(inputStream);
+        JsonElement rootElement = parser.parse(reader);
+        JsonObject rootObject = rootElement.getAsJsonObject();
+        JsonObject pages = rootObject.getAsJsonObject("query").getAsJsonObject("pages");
+        JsonArray array = null;
+        for (Map.Entry<String, JsonElement> entry : pages.entrySet()) {
+            JsonObject entryObject = entry.getValue().getAsJsonObject();
+            array = entryObject.getAsJsonArray("revisions");
+        }
+        JsonObject revision = array.get(0).getAsJsonObject();
+        String intended = revision.get(field).getAsString();
+        return intended;
+    }
 }
---
+++ src/test/java/edu/bsu/cs222/JsonLearningTest.java
@@ -0,0 +1,75 @@
+package edu.bsu.cs222;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Map;
+
+public class JsonLearningTest {
+
+    @Test
+    public void testCountRevisions() {
+        JsonParser parser = new JsonParser();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("testResource.json");
+        Reader reader = new InputStreamReader(inputStream);
+        JsonElement rootElement = parser.parse(reader);
+        JsonObject rootObject = rootElement.getAsJsonObject();
+        JsonObject pages = rootObject.getAsJsonObject("query").getAsJsonObject("pages");
+        JsonArray array = null;
+        for (Map.Entry<String, JsonElement> entry : pages.entrySet()) {
+            JsonObject entryObject = entry.getValue().getAsJsonObject();
+            array = entryObject.getAsJsonArray("revisions");
+        }
+        Assert.assertEquals(5, array.size());
+    }
+
+    @Test
+    public void testReturnsUser() {
+        JsonParser parser = new JsonParser();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("testResource.json");
+        Reader reader = new InputStreamReader(inputStream);
+        JsonElement rootElement = parser.parse(reader);
+        JsonObject rootObject = rootElement.getAsJsonObject();
+        JsonObject pages = rootObject.getAsJsonObject("query").getAsJsonObject("pages");
+        JsonArray array = null;
+        for (Map.Entry<String, JsonElement> entry : pages.entrySet()) {
+            JsonObject entryObject = entry.getValue().getAsJsonObject();
+            array = entryObject.getAsJsonArray("revisions");
+        }
+        JsonObject revision = array.get(0).getAsJsonObject();
+        String user = revision.get("user").getAsString();
+        Assert.assertEquals("FrescoBot", user);
+    }
+
+    @Test
+    public void testReturnsTimestamp() {
+        JsonParser parser = new JsonParser();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("testResource.json");
+        Reader reader = new InputStreamReader(inputStream);
+        JsonElement rootElement = parser.parse(reader);
+        JsonObject rootObject = rootElement.getAsJsonObject();
+        JsonObject pages = rootObject.getAsJsonObject("query").getAsJsonObject("pages");
+        JsonArray array = null;
+        for (Map.Entry<String, JsonElement> entry : pages.entrySet()) {
+            JsonObject entryObject = entry.getValue().getAsJsonObject();
+            array = entryObject.getAsJsonArray("revisions");
+        }
+        JsonObject revision = array.get(1).getAsJsonObject();
+        String timestamp = revision.get("timestamp").getAsString();
+        Assert.assertEquals("2017-08-31T14:34:15Z", timestamp);
+    }
+
+    @Test
+    public void testRevisionParser() {
+        RevisionParser parser = new RevisionParser();
+        String user = parser.parser("user");
+        Assert.assertEquals("FrescoBot", user);
+    }
+}
